78d78c83b0bfb48068e0903301f1f76f7c74ba0a
Jeff <2101120J@student.tp.edu.sg>
2022-12-29 12:39:23 +0800

    Location

---
+++ TravelHub/Shared/Domain/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelHub.Shared.Domain
+{
+    class Location
+    {
+        public int LocationID { get; set; }
+        public String name { get; set; }
+        public String address { get; set; }
+        public int safety { get; set; }
+        public int CityID { get; set; }
+        public virtual City City { get; set; }
+    }
+}
