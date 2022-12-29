b992dfadd7e5e47cdea4d65a2fd19a27d6bfad00
Jeff <2101120J@student.tp.edu.sg>
2022-12-29 12:25:17 +0800

    City and Staff

---
+++ TravelHub/Shared/Domain/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelHub.Shared.Domain
+{
+    public class City
+    {
+        public int CityID { get; set; }
+        public String Name { get; set; }
+        public int Safety { get; set; }
+        public String Transport { get; set; }
+        public String Countryname { get; set; }
+
+    }
+}
---
+++ TravelHub/Shared/Domain/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelHub.Shared.Domain
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public String Name { get; set; }
+        public int Number { get; set; }
+        public String Address { get; set; }
+
+    }
+}
