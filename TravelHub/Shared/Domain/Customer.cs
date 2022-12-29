37cc1a4ddd7d968c656a245b24e08e8fa0f81e04
MuhdSyahir10 <2102392D@student.tp.edu.sg>
2022-12-29 12:21:15 +0800

    customer and itinerary cs files

---
+++ TravelHub/Shared/Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelHub.Shared.Domain
+{
+    public class Customer
+    {
+        public int CustID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ TravelHub/Shared/Domain/Itinerary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelHub.Shared.Domain
+{
+    public class Itinerary
+    {
+        public int ItnID { get; set; }
+        public int Duration { get; set; }
+        public float Budget { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int CustID { get; set; } 
+    }
+}
